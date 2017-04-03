0cd97c779b604dfad858276d8c66bd4f4b7588e8
Linus Lagerhjelm <linus.lagerhjelm@gmail.com>
2017-04-03 14:22:52 +0200

    Manage testing

--- client/src/app/document.service.spec.ts
+++ client/src/app/document.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { DocumentService } from './document.service';
 
+import { Observer, Observable } from './interfaces/observer-observable.interface';
+
 describe('DocumentService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -9,8 +11,12 @@ describe('DocumentService', () => {
     });
   });
 
-  it('should not crash when subscribing without connection', 
-  inject([DocumentService], (service: DocumentService) => {
-    expect(service.subscribe(() => {})).toBeFalsy();
+  it('should open a connection', inject([DocumentService], (service: DocumentService) => {
+    let onSuccess = () => {
+    }
+    service.registerObserver({update:(subject: Observable, action: Object) => {
+      expect(action).toEqual("message");
+    }});
+    service.connect("ws://echo.websocket.org", onSuccess);
   }));
 });
